d4aa3ca26f800a6199e1ddd28b14307ac6f6c92f
yasminstudent <yasminprog02@gmail.com>
2022-12-05 18:58:29 -0300

    trabalhando com rotas

--- 01_NocoesBasicas/src/server.ts
+++ 01_NocoesBasicas/src/server.ts
@@ -6,5 +6,15 @@ server.get('/', (req: Request, res: Response) => {
     res.send("Olá Mundo");
 });
 
+server.get('/noticia/:slug', (req: Request, res: Response) => {
+    let slug: string = req.params.slug;
+    res.send(`Notícia: ${slug}`);
+});
+
+server.get('/voo/:origem-:destino', (req: Request, res: Response) => {
+    let {origem, destino} = req.params;
+    res.send(`Voos de ${origem} a ${destino}`);
+});
+
 //executando o servidor na porta 3000
 server.listen(3000);
