8923fcadc5e4f353809ab238105e4beca3bc5a7d
yasminstudent <yasminprog02@gmail.com>
2022-12-05 19:16:01 -0300

    Criando o 404 (página não encontrada)

--- 01_NocoesBasicas/src/routes/index.ts
+++ 01_NocoesBasicas/src/routes/index.ts
@@ -1,6 +1,6 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response } from "express"
 
-const router = Router();
+const router = Router()
 
 router.get('/', (req: Request, res: Response) => {
     res.send('Olá Mundo!')
@@ -10,4 +10,4 @@ router.get('/eng', (req: Request, res: Response) => {
     res.send('Hello World!')
 });
 
-export default router;
+export default router
--- 01_NocoesBasicas/src/server.ts
+++ 01_NocoesBasicas/src/server.ts
@@ -1,13 +1,17 @@
-import express from 'express';
+import express, {Request, Response} from 'express'
 //Obs: Quando o arquivo tem nome de index, pode colocar só o nome da pasta: ./routes 
-import mainRoutes from './routes/index';
+import mainRoutes from './routes/index'
 
-const server = express();
+const server = express()
 
-server.use(mainRoutes);
+server.use(mainRoutes)
+
+server.use((req: Request, res: Response) => {
+    res.status(404).send('Página não encontrada!');
+});
 
 //pode-se adicionar um préfixo nas rotas:
 //server.use('/prefixo', mainRoutes);
 
 //executando o servidor na porta 3000
-server.listen(3000);
+server.listen(3000)
